4e2bbdf01d5dc92e036abf2cf927d01b49bd280d
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2021-02-13 23:59:39 +0530

    added homepage, login page

--- voting-app/src/App.js
+++ voting-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import { Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="jumbotron centered">
+        <div class="container">
+          <i class="fas fa-key fa-6x"></i>
+          <h1 class="display-3">Voting App</h1>
+          <p class="lead">Register or Login to Vote!!</p>
+          <hr />
+          <Link class="btn btn-light btn-lg" to="/register" role="button">Register</Link>
+          <Link class="btn btn-dark btn-lg" to="/login" role="button">Login</Link>
+        </div>
+      </div>
     </div>
   );
 }
