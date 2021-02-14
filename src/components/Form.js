41a16f0d59232f2e7f526354f1cbb872dbe0117e
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2021-02-15 01:10:24 +0530

    fixed error

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,77 +17,78 @@ function Login({ path, name }) {
         const headers = {
             'Content-Type': 'application/json'
         }
-
+        const response = axios.post(`${BASE_URI}${path}`, userDetails, { headers })
+            .then(function (resp) {
+                console.log(typeof resp.data.status)
+                authenticate(resp, path)
+            })
+            .catch(e => console.log(e))
     }
-    const response = axios.post(`${BASE_URI}${path}`, userDetails, { headers })
-        .then(function (resp) {
-            console.log(typeof resp.data.status)
-            authenticate(resp, path)
-        })
-        .catch(e => console.log(e))
-}
 
-async function authenticate(resp, path) {
-    if (path === '/login') {
-        const status = await resp.data.status;
-        // check user password and username
-        if (status == 200) {
-            // check if he is admin
-            if (resp.data.email === 'admin@gmail.com') {
-                history.push({
-                    pathname: "/admin",
-                    state: {
-                        email: resp.data.email,
-                    }
-                })
-            }//if he is not admin check the user voter or not
-            else if (resp.data.isVoted) {
-                alert("Already Voted!!")
-                history.push("/")
+
+
+
+    async function authenticate(resp, path) {
+        if (path === '/login') {
+            const status = await resp.data.status;
+            // check user password and username
+            if (status == 200) {
+                // check if he is admin
+                if (resp.data.email === 'admin@gmail.com') {
+                    history.push({
+                        pathname: "/admin",
+                        state: {
+                            email: resp.data.email,
+                        }
+                    })
+                }//if he is not admin check the user voter or not
+                else if (resp.data.isVoted) {
+                    alert("Already Voted!!")
+                    history.push("/")
+                } else {
+                    // if he is not voted send for voting 
+                    history.push({
+                        pathname: "/voting",
+                        state: { email: resp.data.email }
+                    })
+                }
+                // if status code is not 200 then user entered wrong password
             } else {
-                // if he is not voted send for voting 
-                history.push({
-                    pathname: "/voting",
-                    state: { email: resp.data.email }
-                })
-            }
-            // if status code is not 200 then user entered wrong password
+                alert("Wrong Credentials")
+                history.push("/");
+            } //if try to access without login redirect to homepage 
         } else {
-            alert("Wrong Credentials")
-            history.push("/");
-        } //if try to access without login redirect to homepage 
-    } else {
-        alert(resp.data.msg)
-        history.push("/")
+            alert(resp.data.msg)
+            history.push("/")
+        }
     }
-}
 
-return (
-    <div className="container mt-5">
-        <h1>{name}</h1>
-        <div className="row">
-            <div className="col-sm-8">
-                <div className="card">
-                    <div className="card-body">
+    return (
+        <div className="container mt-5">
+            <h1>{name}</h1>
+            <div className="row">
+                <div className="col-sm-8">
+                    <div className="card">
+                        <div className="card-body">
 
-                        <form onSubmit={(e) => handleSubmit(e)}>
-                            <div className="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" className="form-control" name="username" id="username" required />
-                            </div>
-                            <div className="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" className="form-control" name="password" id='password' required />
-                            </div>
-                            <button type="submit" className="btn btn-dark mt-4">{name}</button>
-                        </form>
+                            <form onSubmit={(e) => handleSubmit(e)}>
+                                <div className="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" className="form-control" name="username" id="username" required />
+                                </div>
+                                <div className="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" className="form-control" name="password" id='password' required />
+                                </div>
+                                <button type="submit" className="btn btn-dark mt-4">{name}</button>
+                            </form>
 
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
 }
 
 export default Login;
